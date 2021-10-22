c7baa978e8e59814cc4acf49bad2e79e4836d5e1
unknown <dharaneeshram@gmail.com>
2021-10-22 19:58:04 +0530

    Added: To covert morse code to text

--- MorseCode.py
+++ MorseCode.py
@@ -71,19 +71,78 @@ def encrypt(text):
                     ')':'-.--.-'
                 }
     
-    encryted_text = ""
+    decrypted_text = ""
 
     try:
         for char in text:
             if char in morseDict.keys():
-                encryted_text += morseDict[char] + ' '
+                decrypted_text += morseDict[char] + ' '
             else:
-                encryted_text += '/'
+                decrypted_text += '/ '
             
     except:
         pass
 
-    return encryted_text
+    return decrypted_text
+
+def decrypt(code):
+    morseDict = {
+                    '.-':'A', 
+                    '-...':'B',
+                    '-.-.':'C',
+                    '-..':'D', 
+                    '.':'E',
+                    '..-.':'F', 
+                    '--.':'G', 
+                    '....':'H',
+                    '..':'I', 
+                    '.---':'J', 
+                    '-.-':'K',
+                    '.-..':'L', 
+                    '--':'M', 
+                    '-.':'N',
+                    '---':'O', 
+                    '.--.':'P', 
+                    '--.-':'Q',
+                    '.-.':'R', 
+                    '...':'S', 
+                    '-':'T',
+                    '..-':'U', 
+                    '...-':'V', 
+                    '.--':'W',
+                    '-..-':'X', 
+                    '-.--':'Y', 
+                    '--..':'Z',                                        
+                    '----.':'1',                    
+                    '---..':'2',                    
+                    '--...':'3',                    
+                    '-....':'4',                    
+                    '.....':'5',                    
+                    '....-':'6',                    
+                    '...--':'7',                   
+                    '..---':'8',                   
+                    '.----':'9',                   
+                    '-----':'0',                    
+                    '--..--':',',                    
+                    '-.-.-.':'.',                    
+                    '..--..':'?',                    
+                    '.-..-':'/',                   
+                    '-....-':'-',                    
+                    '.--.-':'(',                    
+                    '-.--.-':')',  
+                }
+    
+    decrypted_text = ""
+
+    code_list = code.split(" ")
+
+    for char in code_list:
+        if char in morseDict.keys():
+            decrypted_text += morseDict[char]
+        else:
+            decrypted_text += ' '
+
+    return decrypted_text
 
 def main():
     print("\n########################### MORSE CODE ###########################")
@@ -91,8 +150,17 @@ def main():
     choice = True
 
     while choice:
-        text = input("\nEnter text to be converted into Morse code\n>> ")
-        print("\nMorse code\n>> ", encrypt(text))
+        print("\nConvert to:\n1. Text to Morse code\n2. Morse code to text")
+        oper = input("\nEnter choice(1 or 2)\n>> ")
+
+        if oper == '1':
+            text = input("\nEnter text to be converted into Morse code\n>> ")
+            print("\nMorse code\n>> ", encrypt(text))
+        elif oper == '2':
+            text = input("\nEnter Morse code to be converted into text\n>> ")
+            print("\nMorse code\n>> ", decrypt(text))
+        else:
+            print("Operation unavailable. Enter 1 or 2 only.")
 
         print("\nDo you want to try again? (Y/N)")
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Morse-Code-Converter
 
-Python script to convert a text into [Morse Code]("https://en.wikipedia.org/wiki/Morse_code").
+Python script to convert a text into [Morse Code]("https://en.wikipedia.org/wiki/Morse_code") or Morse code to text.
